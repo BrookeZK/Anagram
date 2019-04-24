89e644a4f65198ecc7eadeb9c58c745cf086a7be
Brooke Kullberg and Darrion Gering <brooke@>
2019-04-24 14:17:08 -0700

    Add list and IsAnagram method

--- Anagram.Tests/ModelTests/WordsTests.cs
+++ Anagram.Tests/ModelTests/WordsTests.cs
@@ -1,11 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Anagram;
+using System;
+using System.Collections.Generic;
 
 namespace Anagram.Tests
 {
   [TestClass]
-  public class WordsTest
+  public class WordsTest : IDisposable
   {
+
+    public void Dispose()
+    {
+      Words.ClearAll();
+    }
+
     [TestMethod]
     public void WordsConstructor_CreatesInstanceOfWords_Words()
     {
@@ -41,6 +49,57 @@ namespace Anagram.Tests
       // Words newWords2 = new Words(word2);
       // char[] result2 = newWords2.MakeArray();
       Assert.AreEqual(result, result);
+    }
+
+    [TestMethod]
+    public void MakeString_CharArrayIntoString_string()
+    {
+      string word = "beard";
+      Words newWords = new Words(word);
+      char[] result = newWords.MakeArray();
+      string newResult = newWords.MakeString(result);
+      Assert.AreEqual(word, newResult);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_WordsList()
+    {
+
+      //Arrange
+      List<Words> newList = new List<Words> {};
+
+      //Act
+      List<Words> result = Words.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsWords_WordsList()
+    {
+      string word1 = "cat";
+      string word2 = "liger";
+      Words newWord1 = new Words(word1);
+      Words newWord2 = new Words(word2);
+      List<Words> newList = new List<Words> { newWord1, newWord2 };
+
+      List<Words> result = Words.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void IsAnagram_CompareCharArrayToMainWord_true();
+    {
+      string word1 = "beard";
+      string word2 = "bread";
+      Words newWord1 = new Words(word1);
+      Words newWord2 = new Words(word2);
+      List<Words> newList = new List<Words> { newWord1, newWord2 };
+
+      List<Words> result = Words.IsAnagram(newList);
+
     }
   }
 }
